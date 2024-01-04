8b577243932392d5e150975c40a5e076d863d5c6
akhil-haridas <4khilharidas@gmail.com>
2024-01-04 21:12:09 +0530

    Header component with image

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+      <div className="flex items-center ">
+          <img src='https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg' className="w-28 h-28" />
+          <p className="mx-4">+</p>
+          <img src='https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-1024.png' className="w-28 h-28"/>
+    </div>
+  )
+}
+
+export default Header
