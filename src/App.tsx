1d7627ad0055fb11ee0c1db4e173c0170bde6bdd
akhil-haridas <4khilharidas@gmail.com>
2024-01-04 21:22:46 +0530

    layout of the todo list

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ function App() {
   return (
     <>
       <div className='h-[100vh] flex flex-col justify-center items-center'>
-        <h1 className='text-3xl'>
+        <h1 className='text-3xl p-10 font-bold text-blue-400'>
         React Todo-List
         </h1>
-        <Header/>
+        <Header />
+        <div className='w-[350px] flex items-center flex-col p-10'>
+          <input type='text' className='w-full'/>
+          <button type='submit' className='w-full bg-blue-950 p-1 m-3 text-white'>ADD</button>
+        </div>
       </div>
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
       <div className="flex items-center ">
           <img src='https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg' className="w-28 h-28" />
-          <p className="mx-4">+</p>
+          <p className="mx-4 font-bold text-2xl text-white">+</p>
           <img src='https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-1024.png' className="w-28 h-28"/>
     </div>
   )
