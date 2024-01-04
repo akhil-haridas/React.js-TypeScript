f02d507de06f26825f44c2f0bb9045625c8f6200
akhil-haridas <4khilharidas@gmail.com>
2024-01-04 21:11:39 +0530

    used header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className=''>
+      <div className='h-[100vh] flex flex-col justify-center items-center'>
+        <h1 className='text-3xl'>
         React Todo-List
+        </h1>
+        <Header/>
       </div>
     </>
   )
