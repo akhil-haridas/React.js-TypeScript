b339f2fafb96ee8eb3cac1b5d5b23903949644fc
akhil-haridas <4khilharidas@gmail.com>
2024-01-06 15:49:13 +0530

    Created list component

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,7 @@ function App() {
     setInputValue("");
   };
 
-  const handleDelete = (value: string) => {
-    setItems((prev) => prev.filter((data) => data.id !== value));
-  };
+
   return (
     <>
       <div className="h-[100vh] flex flex-col justify-center items-center">
@@ -41,25 +39,7 @@ function App() {
             Add
           </Button>
           <section className="h-52 overflow-auto w-full ">
-            {items.map((data) => (
-              <div
-                key={data.id}
-                className="flex justify-between py-2 border border-slate-200 pl-2 mb-2 mr-1"
-              >
-                <p className="text-white">{data.title}</p>
-                <Button onClick={() => handleDelete(data.id)}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>
-                    <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
-                  </svg>
-                </Button>
-              </div>
-            ))}
+
           </section>
         </form>
       </div>
