0e23053e45c126637f2285b8f56eab32ecc622ea
akhil-haridas <4khilharidas@gmail.com>
2024-01-06 15:35:51 +0530

    set type  button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
-export const Button = () => {
+type Button = {
+    children: JSX.Element;
+    className:string
+}
+
+export const Button = ({ children, className }:Button) => {
   return (
-    <button type="submit" className="w-full bg-blue-950 p-1 m-3 text-white">
-      ADD
+      <button type="submit" className={`bg-blue-950 p-1 m-3 text-white ${className}`}>
+      {children}
     </button>
   );
 };
